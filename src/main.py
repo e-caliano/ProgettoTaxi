9794a1323d7f0471fd2c6983aaedf4f128d10464
e-caliano <e.caliano2@gmail.com>
2023-02-21 16:16:59 +0100

    modifica function lettura file

--- src/main.py
+++ src/main.py
@@ -2,10 +2,22 @@ import pandas as pd
 
 # funzione che crea un dataframe passando il path (eventualmente assoluto) del file
 def load_file(path):
-    df = pd.read_parquet(path, engine='pyarrow')
+    # estraiamo l'estensione del file, split prende l'ele
+    extension = path.split(".")[-1]
+
+    if extension == "csv":
+        # se l'estensione è csv, carichiamo il DataFrame dal file CSV
+        df = pd.read_csv(path)
+    elif extension == "parquet":
+        # se l'estensione è parquet, carichiamo il DataFrame dal file PARQUET
+        df = pd.read_parquet(path, engine='pyarrow')
+    else:
+        # se l'estensione non è supportata, solleviamo un'eccezione
+        raise ValueError("Estensione del file non supportata. Sono supportati solo i file CSV e PARQUET.")
+
     return df
 
-x = load_file('/Users/edoardocaliano/Desktop/PrgettoTaxi/Data/yellow_tripdata_2022-03.parquet')
+
 
 
 
