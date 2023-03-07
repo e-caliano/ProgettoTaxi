c762fceef08e48e5f968a0edfb4d6ba81f405952
Gaetano <gaetanof1707@gmail.com>
2023-03-07 17:29:48 +0100

    Inserimento file main_ProgettoTaxi

--- risorse/input.py
+++ risorse/input.py
@@ -1,13 +1,7 @@
 import pandas as pd
 import os
-from elaborazione import merge_and_filter
-from elaborazione import durata
-from elaborazione import merge_and_filter_per_tutti_i_quartieri
-from elaborazione import min_max_durata
-from post_elaborazione import export_to_csv
-from post_elaborazione import export_min_max_durata_to_csv
-import matplotlib.pyplot as plt
-from plot import plot_durata_massima_per_quartiere
+
+
 
 def carica_informazioni(anno, mese, path):
     nome_file = f'yellow_tripdata_{anno}-{mese}.parquet'
@@ -38,12 +32,4 @@ def richiesta_utente():
 
 
 
-df_list,quartieri_list = richiesta_utente()
-merged_list = merge_and_filter(df_list, quartieri_list)
-x=durata(merged_list)
-merged_list2 = merge_and_filter_per_tutti_i_quartieri(df_list)
-y = durata(merged_list2)
-z,t = min_max_durata(y)
-export_to_csv(x, 'Durate_per_quartiere_cercato_ufficiale')
-export_min_max_durata_to_csv(z,t,'Durate_minima_e_massima')
-plot_durata_massima_per_quartiere(y, 'Istogramma')
+
